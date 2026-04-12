9f39193e47201c81e1782cb058876018d5ff675a
Oksana_Nesterova <nox23061992@gmail.com>
2026-04-12 22:58:07 +0300

    Added Article model and implemented migrations

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    name = models.CharField(max_length=200) #название статьи
+    body = models.TextField() #тело статьи
+    created_at = models.DateTimeField(auto_now_add=True) #для хранения даты и времени создания записи
+    updated_at = models.DateTimeField(auto_now=True) #для хранения даты и времени последнего изменения записи
