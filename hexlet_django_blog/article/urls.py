4f8cf0abda708c7d7509f4e35d1bd253613bf017
Oksana_Nesterova <nox23061992@gmail.com>
2026-04-13 22:15:46 +0300

    Implemented list (CRUD)
    A route has been added, a view handler has been implemented, and data has been displayed in the template

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from hexlet_django_blog.article import views
+from hexlet_django_blog.article.views import IndexView
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name='index'),
-    path("articles/<str:tags>/<int:article_id>", views.IndexView.as_view(), name='article'),
+    path("", IndexView.as_view()),
+    path("articles/<str:tags>/<int:article_id>", IndexView.as_view(), name='article'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,17 +1,15 @@
 from django.views import View
 from django.shortcuts import render
 
+from hexlet_django_blog.article.models import Article
+
 class IndexView(View):
-    template_name = "articles/index.html"
-
     def get(self, request, *args, **kwargs):
-        tags = kwargs.get('tags', 'python')
-        article_id = kwargs.get('article_id', 42)
-
-        message = f'Статья номер {article_id}. Тег {tags}'
-        context = {
-            "app_name": "Статьи",
-            "message": message,
-        }
-        return render(request, self.template_name, context)
-
+        articles = Article.objects.all()[:15]
+        return render(
+            request,
+            "articles/index.html",
+            context={
+                "articles": articles,
+            },
+        )
