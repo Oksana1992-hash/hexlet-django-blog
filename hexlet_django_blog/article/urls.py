2bab6a5bd8f4bb8243ada355270df2a6a1b27d41
Oksana_Nesterova <nox23061992@gmail.com>
2026-04-15 14:09:37 +0300

    Implemented updating of entities (CRUD)
    Links for editing each article, updating articles through the interface, and validating them have been added to the article list

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleFormCreateView
+from hexlet_django_blog.article.views import (
+    IndexView,
+    ArticleView,
+    ArticleFormCreateView,
+    ArticleFormEditView,
+)
 
 app_name = 'articles'
 
 urlpatterns = [
     path("", IndexView.as_view(), name='index'),
     path("<int:id>/", ArticleView.as_view(), name='article_detail'),
-    path("create/", ArticleFormCreateView.as_view(), name='articles_create')
+    path("create/", ArticleFormCreateView.as_view(), name='articles_create'),
+    path("<int:id>/edit/", ArticleFormEditView.as_view(), name='articles_update'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -36,6 +36,26 @@ class ArticleFormCreateView(View):
     def post(self, request, *args, **kwargs):
         form = ArticleForm(request.POST)
         if form.is_valid():
-            article = form.save()
-            return redirect('articles:index')
+            form.save()
+            return redirect("articles:index")
         return render(request, "articles/create.html", {"form": form})
+
+class ArticleFormEditView(View):
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get("id")
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        return render(
+            request, "articles/update.html", {"form": form, "article_id": article_id}
+        )
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get("id")
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect("articles:index")
+        return render(
+            request, "articles/update.html", {"form": form, "article_id": article_id}
+        )
